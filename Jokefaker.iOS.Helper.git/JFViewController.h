fa8ee3b381c10e8f7c15f00fab97caefc974e375
周国勇 <zhouguoyong@shuukokuyuus-MacBook-Pro.local>
2014-08-14 17:47:55 +0800

    Init 0.0.1

--- Jokefaker.iOS.Helper.git/JFViewController.h
+++ Jokefaker.iOS.Helper.git/JFViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CommonCategory.h"
 
 @interface JFViewController : UIViewController
 
---
+++ Jokefaker.iOS.Helper.git/Libs/Category/CommonCategory.h
@@ -0,0 +1,19 @@
+//
+//  CommonCategory.h
+//  Jokefaker.iOS.Helper.git
+//
+//  Created by 周国勇 on 8/14/14.
+//  Copyright (c) 2014 jokefaker. All rights reserved.
+//
+
+#ifndef Jokefaker_iOS_Helper_git_CommonCategory_h
+#define Jokefaker_iOS_Helper_git_CommonCategory_h
+
+#import "NSString+CC.h"
+#import "NSDate+CC.h"
+#import "NSData+CC.h"
+#import "NSNumber+CC.h"
+#import "NSTimer+CC.h"
+#import "NSArray+CC.h"
+
+#endif
